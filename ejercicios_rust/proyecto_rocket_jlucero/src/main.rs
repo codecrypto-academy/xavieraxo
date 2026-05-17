e4441f7574eb5bcbb5a8f1845f4bcdd2b59c5e45
xavieraxo <lucerojavier@live.com>
2026-05-17 17:54:49 -0300

    Tarea #3 Trabajo con Query String

--- ejercicios_rust/proyecto_rocket_jlucero/src/main.rs
+++ ejercicios_rust/proyecto_rocket_jlucero/src/main.rs
@@ -17,7 +17,25 @@ fn ger_item(id: u32, nombre: &str, email: &str) -> String {
     format!("ID recibido: {id}, Nombre: {nombre}, Email: {email}")
 }
 
+
+/// GET con query string: /lista?page=1&registros=10&orden=asc
+#[get("/lista?<tennant>&<person_name>&<page>&<registros>&<orden>")]
+fn lista(tennant: Option<String>, person_name: Option<String>, page: Option<u32>, registros: Option<u32>, orden: Option<String>) -> String {
+    let page = page.unwrap_or(1);
+    let registros = registros.unwrap_or(10);
+    let orden = orden.unwrap_or_else(|| String::from("id"));
+    format!("Tennant: {:?}, Person Name: {:?}, Página: {}, Items por página: {}, Sorted by: {}", tennant, person_name, page, registros, orden)
+}
+
+
+/// GET con vector de strings: /tags?valor=rust&valor=rocket&valor=web
+#[get("/itemsMultiple?<ids>")]
+fn get_multiples_ids(ids: Vec<String>) -> String {
+
+    format!("IDs recibidos: {:?}", ids)
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, hola, ger_item])
+    rocket::build().mount("/", routes![index, hola, ger_item, lista, get_multiples_ids])
 }
